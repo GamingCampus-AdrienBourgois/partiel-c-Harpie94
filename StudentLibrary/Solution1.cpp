0a6d699ea9cf54a45767cbd62b5a6c832defdfdc
Harpie94 <rponsignon@gaming.tech>
2024-01-19 14:32:05 +0100

    Ex1 Test1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -6,7 +6,44 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
-}
+            switch (_from)
+            {
+            case TemperatureUnits::CELSIUS:
+                if (_to == TemperatureUnits::FAHRENHEIT)
+                {
+                    return (_value * 9.0 / 5.0) + 32.0;
+                }
+                else if (_to == TemperatureUnits::KELVIN)
+                {
+                    return _value + 273.15;
+                }
+                break;
+
+            case TemperatureUnits::FAHRENHEIT:
+                if (_to == TemperatureUnits::CELSIUS)
+                {
+                    return (_value - 32.0) * 5.0 / 9.0;
+                }
+                else if (_to == TemperatureUnits::KELVIN)
+                {
+                    return (_value - 32.0) * 5.0 / 9.0 + 273.15;
+                }
+                break;
+
+            case TemperatureUnits::KELVIN:
+                if (_to == TemperatureUnits::CELSIUS)
+                {
+                    return _value - 273.15;
+                }
+                else if (_to == TemperatureUnits::FAHRENHEIT)
+                {
+                    return (_value - 273.15) * 9.0 / 5.0 + 32.0;
+                }
+                break;
+            }
+
+            // If units are not recognized, return an error value
+            return -1.0;
+    };
 
 #endif
